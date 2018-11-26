54ce2886ca519b04ff60b4014687a9e54085373b
Olivier DOSSMANN <git@dossmann.net>
2018-11-26 17:03:05 +0100

    Couche logique : création de la couche logique + adaptation couche physique

---
+++ zos/cobol/PROSLSEL.cbl
@@ -0,0 +1,27 @@
+      *****************************************************************
+      *                   C O U C H E  L O G I Q U E                   
+      *                   --------------------------
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.      PROSBSEL.
+       AUTHOR.          Olivier DOSSMANN.
+       DATE-WRITTEN.    20181126.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. ZIA.
+       OBJECT-COMPUTER. VIRTEL.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      *   Couche physique
+       01 w-parametres.
+      *        Fonction choisie
+            05 w-parametres-fx PIC X(6).
+      *   Code retour
+       01 w-rc                 PIC X(2).
+       PROCEDURE DIVISION USING w-parametres w-rc.
+       DEBUT.
+      *    Envoi d'une demande de SELECT a la couche physique
+           MOVE 'SELECT'   TO    w-parametres-fx.
+           CALL 'PROSPSEL' USING w-parametres.
+       END PROGRAM PROSPBSEL.
--- zos/cobol/PROSPSEL.cbl
+++ zos/cobol/PROSPSEL.cbl
@@ -2,27 +2,36 @@
       * Couche physique (acces aux donnees)
       *****************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. PROSPSEL.
-       AUTHOR. Olivier DOSSMANN.
-       DATE-WRITTEN. 20181122.
+       PROGRAM-ID.      PROSPSEL INITAL.
+       AUTHOR.          Olivier DOSSMANN.
+       DATE-WRITTEN.    20181122.
       
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. ZIA.
        OBJECT-COMPUTER. VIRTEL.
        DATA DIVISION.
+       LINKAGE SECTION.
+      *   Code retour du programme appelant
+       77 code-retour       PIC X(2).
+      *   Parametres envoyes par le programme appelant
+       01 parametres.
+      *       Fonction appelee
+           05 parametres-fx PIC X(5).
+
        WORKING-STORAGE SECTION.
-       77 lecture-eot PIC 9 VALUE 0.
+       77 lecture-eot       PIC  9 VALUE 0.
            EXEC SQL
              INCLUDE SQLCA
            END-EXEC.
            EXEC SQL
              INCLUDE PROSCPRO
            END-EXEC.
-       PROCEDURE DIVISION.
+       PROCEDURE DIVISION using parametres code-retour.
        DEBUT.
+           DISPLAY 'CP - Parametres: ' parametres-fx.
            PERFORM LECTURE-INIT.
-           PERFORM LECTURE-TRT UNTIL lecture-eot = 1.
+           PERFORM LECTURE-TRT      UNTIL lecture-eot = 1.
            PERFORM LECTURE-FIN.
            STOP RUN.
        LECTURE-INIT.
